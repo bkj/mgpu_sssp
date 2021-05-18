64d82b90cce2c8f7b6dbe4b23bfa0aa1afc34bf5
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2021-05-18 13:04:30 +0000

    cleaning up convergence criteria

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # run.sh
 
-PROB=rmat22.bin
+PROB=rmat24.bin
 
 # --
 # Build
@@ -14,7 +14,7 @@ make main -j4
 # Run
 
 CUDA_VISIBLE_DEVICES=0        ./main data/$PROB
-# CUDA_VISIBLE_DEVICES=0,1      ./main data/$PROB
-# CUDA_VISIBLE_DEVICES=0,1,2    ./main data/$PROB
-# CUDA_VISIBLE_DEVICES=0,1,2,3  ./main data/$PROB
+CUDA_VISIBLE_DEVICES=0,1      ./main data/$PROB
+CUDA_VISIBLE_DEVICES=0,1,2    ./main data/$PROB
+CUDA_VISIBLE_DEVICES=0,1,2,3  ./main data/$PROB
 
